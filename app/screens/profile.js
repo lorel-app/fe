49cb8e7be58d2c9f75455061f7764689cb896eb9
Alsje15 <alsje154@gmail.com>
2024-09-06 15:40:04 +0200

    chore: 🤸‍♀️ update ProfileScreen to use authContext

--- app/screens/profile.js
+++ app/screens/profile.js
@@ -1,77 +1,93 @@
 import { View, TouchableOpacity, Text, Alert, Image } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useContext } from "react";
 import { useGlobalStyles } from "@/hooks/useGlobalStyles";
 import { useMediaPicker } from "@/hooks/useMediaPicker";
 import api from "@/utils/api";
 import SignUpLogInModal from "@/app/Auth";
+import AuthContext from "@/utils/authContext";
 
 const ProfileScreen = () => {
   const styles = useGlobalStyles();
-  const [user, setUser] = useState(null);
-   const [displayPicture, setDisplayPicture] = useState(null);
+  const [displayPicture, setDisplayPicture] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const { isAuthenticated, user, loadUser } = useContext(AuthContext);
   const { image, pickImage } = useMediaPicker();
 
   const fetchUser = useCallback(async () => {
-    const me = await api.getMe();
-    if (me) {
-      setUser(me.username);
-      setDisplayPicture(me.displayPicture);
-    } else {
-      setModalVisible(true);
+    if (isAuthenticated && user) {
+      setDisplayPicture(user.displayPicture);
     }
-  }, []);
+  }, [isAuthenticated, user]);
 
   useEffect(() => {
-    fetchUser();
-  }, [fetchUser]);
+    if (isAuthenticated) {
+      fetchUser();
+    }
+  }, [isAuthenticated, fetchUser]);
 
-    useEffect(() => {
-      const uploadImage = async () => {
-        if (image) {
-          try {
-            const response = await api.updateProfilePic(image);
-            if (response.success) {
-              Alert.alert("Success", "Profile picture updated successfully");
-              fetchUser(); 
-            } else {
-              Alert.alert(
-                "Error",
-                response.error || "Failed to update profile picture"
-              );
-            }
-          } catch (error) {
-            Alert.alert("Error", "An error occurred during the upload");
+  useEffect(() => {
+    const uploadImage = async () => {
+      if (image) {
+        try {
+          const response = await api.updateProfilePic(image);
+          if (response.success) {
+            Alert.alert("Success", "Profile picture updated successfully");
+            const newDisplayPicture = response.data.displayPicture.medium;
+            setDisplayPicture(newDisplayPicture);
+          } else {
+            Alert.alert(
+              "Error",
+              response.error || "Failed to update profile picture"
+            );
           }
+        } catch (error) {
+          Alert.alert("Error", "An error occurred during the upload");
         }
-      };
+      }
+    };
 
-      uploadImage();
-    }, [image]);
+    uploadImage();
+  }, [image, fetchUser]);
 
   return (
-    <View style={styles.post}>
-      <View style={styles.rowSpan}>
-        <TouchableOpacity style={styles.row}>
-          {displayPicture ? (
-            <Image source={{ uri: displayPicture }} resizeMode="cover" style={styles.myProfilePic} />
-          ) : (
-            <Icon name="circle" size={100} style={styles.myProfilePic} />
-          )}
-          {user ? <Text style={styles.text}>{user}</Text> : null}
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.row} onPress={pickImage}>
-          <Text>Edit Profile Picture</Text>
-        </TouchableOpacity>
-      </View>
+    <View style={styles.container}>
+      {isAuthenticated ? (
+        <View style={styles.rowSpan}>
+          <TouchableOpacity style={styles.row}>
+            {displayPicture ? (
+              <Image
+                source={{ uri: displayPicture }}
+                resizeMode="cover"
+                style={styles.myProfilePic}
+              />
+            ) : (
+              <Icon name="circle" size={100} style={styles.myProfilePic} />
+            )}
+            {user ? <Text style={styles.text}>{user.username}</Text> : null}
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.row} onPress={pickImage}>
+            <Text>Edit Profile Picture</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View>
+          <Text style={styles.title}>Please log in or create an account</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setModalVisible(true)}
+          >
+            <Text style={styles.buttonText}>Sign Up / Log In</Text>
+          </TouchableOpacity>
+        </View>
+      )}
       <SignUpLogInModal
         visible={modalVisible}
         onClose={() => setModalVisible(false)}
-        onLoginSuccess={fetchUser}
+        onLoginSuccess={loadUser}
       />
     </View>
   );
 };
 
-export default ProfileScreen
+export default ProfileScreen;
